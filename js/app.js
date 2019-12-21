fe4296311ed9740ac3fefdb168b77c6189c51ccc
Adam Shaul <adamshaul360@gmail.com>
2019-12-21 17:20:42 +0200

    responsive

--- js/app.js
+++ js/app.js
@@ -18,11 +18,6 @@ categories.forEach((category) => {
 
 
 // Popper
-
-
-
-
-
 var refs = document.querySelectorAll('.icon');
 var popup = document.querySelector('.modal');
 var closeBtn = document.querySelector('.close');
@@ -36,7 +31,7 @@ refs.forEach((ref) => {
       placement: 'bottom',
       onCreate: function(data) {
         console.log(data)
-      },
+},
       modifiers: {
         offset: {
           enabled: true,
@@ -53,3 +48,53 @@ refs.forEach((ref) => {
     })
   })
 })
+
+
+// Drag and Drop.
+const elements = document.querySelectorAll('.btn');
+const stage = document.querySelector('.stage');
+let element = null;
+
+console.log('stage', stage)
+// Elements Listener
+elements.forEach((element) => {
+    element.addEventListener('dragstart', dragStart);
+    element.addEventListener('dragend', dragEnd);
+})
+
+// Stage Listeners
+stage.addEventListener('dragover', dragOver);
+stage.addEventListener('dragenter', dragEnter);
+stage.addEventListener('dragleave', dragleave);
+stage.addEventListener('drop', dragDrop);
+
+// Drag Functions
+function dragStart(e) {
+    console.log('START');
+    this.className += ' hold';
+    element = e.target;
+}
+function dragEnd() {
+    console.log('END');
+    stage.className = 'stage';
+    this.className = 'btn';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    console.log(e.type + " : dragging over the stage");
+}
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+function dragleave(e) {
+    this.className = 'stage';
+}
+function dragDrop(e) {
+    console.log(e.type + " : dropped the item to the stage");
+    const element_copy = element.cloneNode();
+    element_copy.innerText = element.innerText
+    stage.append(element_copy);
+    element = null;
+}
