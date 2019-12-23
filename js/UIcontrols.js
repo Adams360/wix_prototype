f5a595bdffdbcd0f6901cf7ac9600b38796d04ff
= <=>
2019-12-23 19:46:20 +0200

    fixing transition on panels

--- js/UIcontrols.js
+++ js/UIcontrols.js
@@ -5,16 +5,33 @@ const panel = document.getElementById('panel');
 const returnBtn = document.querySelector('.returnBtn');
 const hideControlsBtn = document.querySelector('.hideControlsBtn');
 const leftBar = document.querySelector('.leftbar');
+const menuItems = document.querySelectorAll('.main-menu__item');
+
 
 addElementsBtn.addEventListener('click', () => {
   mainMenuWrapper.classList.add('move-out');
   panel.classList.add('move-in');
+
+      menuItems.forEach((menuItem) => {
+        menuItem.classList.add('clipped');
+      })
+
 });
 
 returnBtn.addEventListener('click', () => {
     mainMenuWrapper.classList.remove('move-out');
     panel.classList.remove('move-in');
+
+   menuItems.forEach((menuItem) => {
+       leftBar.addEventListener('transitionend', removedTransition);
+       menuItem.classList.remove('clipped');
+      })
 });
+
+function removedTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  console.log(this);
+}
 
 hideControlsBtn.addEventListener('click', () => {
 
